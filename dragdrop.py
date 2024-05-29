0ad69d32bbf7806e99233a86812707a47b9576a6
Victoria <tovictoriadao@gmail.com>
2024-05-29 16:08:56 -0400

    Checkpoint 2: Starting to add the drag feature. Currently tracking the position of the mouse and button. When mouse is on top of box, active_box will be toggled

--- dragdrop.py
+++ dragdrop.py
@@ -17,7 +17,8 @@ pygame.display.set_caption('Tester')
 #Defines when the box is being moved 
 active = None
 
-#creating a single box
+#Creates a rectangle (left, top, width, height)
+#We will call this box 1
 box = pygame.Rect(20,20,20,20)
 
 #toggles when game is on or off
@@ -29,12 +30,24 @@ while run:
     #Draw the box
     pygame.draw.rect(screen, "purple", box)
 
-    #When the player ends the game, change run to False to end loop
+    #Event = Checks any interaction with the screen: Exiting, MouseDown, Mouse Click
     for event in pygame.event.get():
+        #When the LEFT mouse button is click and held down
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1: #Signifies the left mouse button being down
+              #Check if the box collides with the mouse/clicked by mouse
+              if box.collidepoint(event.pos): #Pos, the position of the box
+                  #Toggle the box to be active.
+                  active_box = 1
+        #Tracks the location of the mouse
+        if event.type == pygame.MOUSEMOTION:
+            print(event)
         if event.type == pygame.QUIT:
             run = False
 
+    #Update the contents of the WHOLE screen
     pygame.display.flip()
+    #pygame.display.update() can update a portion or the whole screen depending on parameters
 
 #End the program
 pygame.quit()
