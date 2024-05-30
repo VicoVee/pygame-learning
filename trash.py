c85edb62959ff8a69ac90a63c83dcdd29b627762
Victoria <tovictoriadao@gmail.com>
2024-05-30 11:01:19 -0400

    Set up the basis of the game by creating the window, a solid background, and toggle game on/off

--- trash.py
+++ trash.py
@@ -1,1 +1,22 @@
+import pygame
+import random
 
+pygame.init()
+
+#Game Window Setup
+win = pygame.display.set_mode((500,500))
+pygame.display.set_caption('Trash Collecting')
+
+#Background
+sand = pygame.Color("#FFF0AD")
+black = pygame.Color(0,0,0)
+
+run = True
+while run:
+    win.fill(sand)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.flip()
+pygame.quit()
