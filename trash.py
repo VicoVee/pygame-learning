0b466ce9a189a88cf14aac9debc235413903155b
Victoria <tovictoriadao@gmail.com>
2024-05-31 11:52:13 -0400

    Added trash sprites (purple rectangles) by creating a list, assigning random dimensions and locations, and then drawing these sprites in the field. In addition, I renamed the variables of the character sprite (x,y,widht,height) to be more specific due to the new trash sprites

--- trash.py
+++ trash.py
@@ -12,19 +12,36 @@ pygame.display.set_caption('Trash Collecting')
 #Background Colors
 sand = pygame.Color("#FFF0AD")
 black = pygame.Color(0,0,0)
+purple = pygame.Color("#A115B3")
 
 #Character Location + Dimensions
     #Character (x-coor, y-coord, width, height)
     #NOTE: Left-Right starts from 0 to 500
     #Up-Down starts from 0 to 500 as well
-x = 50
-y = 420
-width = 100
-height = 100
-speed = 40
+char_x = 50
+char_y = 460
+char_width = 50
+char_height = 100
+char_speed = 40
+
+#Window Boundaries
+widthBoundary = ScreenWidth - char_x - char_speed
+
+#Make a list of interactive trash sprites
+TrashPile= []
+for i in range(5):
+    #Randomly assign a size and location of each piece of trash
+    trash_x = random.randint(5, ScreenWidth - 200)
+    trash_y = random.randint(5, ScreenHeight - 200)
+    trash_w = random.randint(20, 50)
+    trash_h = random.randint(20, 80)
+    #Create and add them to the TrashPile list
+    trash = pygame.Rect(trash_x,trash_y,trash_w,trash_h)
+    TrashPile.append(trash)
 
 run = True
 while run:
+    #Delay the game loading
     pygame.time.delay(100)
 
     for event in pygame.event.get():
@@ -36,21 +53,24 @@ while run:
         keys = pygame.key.get_pressed()
 
         #Checks the key and moves the character correspondingly
-        if keys[pygame.K_a] and x > speed:
-            x -= speed
-        elif keys[pygame.K_d] and x < ScreenWidth - width - speed:
-            print(x)
-            x += speed
-        elif keys[pygame.K_w] and y > speed:
-            y -= speed
-        elif keys[pygame.K_s] and y < ScreenHeight - height - speed:
-            y += speed
+        if keys[pygame.K_a] and char_x > char_speed:
+            char_x -= char_speed
+        elif keys[pygame.K_d] and char_x < ScreenWidth - char_width - char_speed:
+            char_x += char_speed
+        elif keys[pygame.K_w] and char_y > char_speed:
+            char_y -= char_speed
+        elif keys[pygame.K_s] and char_y < ScreenHeight - char_height - char_speed:
+            char_y += char_speed
 
     #Load Background
     win.fill(sand)
 
-    avatar = pygame.Rect(x, y, width, height)
-    pygame.draw.rect(win, black, avatar)
+   #Load the trash in
+    for trash in TrashPile:
+        pygame.draw.rect(win, purple, trash)
+
+    char = pygame.Rect(char_x, char_y, char_width, char_height)
+    pygame.draw.rect(win, black, char)
 
     pygame.display.flip()
 pygame.quit()
